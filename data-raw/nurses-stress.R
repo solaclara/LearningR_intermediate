d364eff7d13f10cd1e4f62f94c59bd63e1ce5ef9
solaclara <sola.clara@gmail.com>
2026-05-05 12:04:17 +0200

    data

--- data-raw/nurses-stress.R
+++ data-raw/nurses-stress.R
@@ -1,3 +1,7 @@
 ## code to prepare `nurses-stress` dataset goes here
 
 usethis::use_data(nurses-stress, overwrite = TRUE)
+# The dataset can be downloaded manually from
+# <https://drive.proton.me/urls/ZHZ6TCAN2W#e5D5U4IajdYp>.
+# Make sure to save this file to the `data-raw/` folder with the name
+# `nurses-stress.tar`.
